3dd93579dfbddadf855596765ec095fa33db1071
Tyler Reckart <tyler@alley.co>
2020-09-26 12:27:38 -0400

    minify js & css bundles

--- src/buildAssets.js
+++ src/buildAssets.js
@@ -1,47 +1,46 @@
 const config = require("../config");
 const fs = require("fs");
 const colors = require("colors");
+const minify = require('@node-minify/core');
+const cleanCSS = require('@node-minify/clean-css');
+const uglifyES = require('@node-minify/uglify-es');
 
 const { outdir, theme } = config;
 
 function buildAssets() {
-  const stylesheet = fs.readFileSync(
-    __dirname + `/themes/${theme}/assets/css/style.css`,
-    "utf8"
-  );
+  if (!fs.existsSync(`${outdir}/css`)) {
+    fs.mkdirSync(`${outdir}/css`);
+  }
 
-  if (stylesheet) {
-    if (!fs.existsSync(`${outdir}/css`)) {
-      fs.mkdirSync(`${outdir}/css`);
-    }
-
-    fs.writeFile(`${outdir}/css/styles.css`, stylesheet, (error) => {
-      if (error) {
-        throw error;
+  minify({
+    compressor: cleanCSS,
+    input: __dirname + `/themes/${theme}/assets/css/style.css`,
+    output: `${outdir}/css/styles.css`,
+    callback: (err, min) => {
+      if (err) {
+        throw err;
       }
-
+  
       console.log(`${"asset =>".magenta} styles.css built`);
-    });
-  }
-
-  const jsBundle = fs.readFileSync(
-    __dirname + `/themes/${theme}/assets/js/index.js`,
-    "utf8"
-  );
-
-  if (jsBundle) {
-    if (!fs.existsSync(`${outdir}/js`)) {
-      fs.mkdirSync(`${outdir}/js`);
     }
+  });
 
-    fs.writeFile(`${outdir}/js/bundle.js`, jsBundle, (error) => {
-      if (error) {
-        throw error;
-      }
-
-      console.log(`${"asset =>".magenta} bundle.js built`);
-    });
+  if (!fs.existsSync(`${outdir}/js`)) {
+    fs.mkdirSync(`${outdir}/js`);
   }
+
+  minify({
+    compressor: uglifyES,
+    input: __dirname + `/themes/${theme}/assets/js/index.js`,
+    output: `${outdir}/js/bundle.js`,
+    callback: (err, min) => {
+      if (err) {
+        throw err;
+      }
+  
+      console.log(`${"asset =>".magenta} bundle.js built`);
+    }
+  });
 }
 
 module.exports = buildAssets;
