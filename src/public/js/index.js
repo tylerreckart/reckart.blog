867c4c8fb767bb9a9b28c4ccaef7e527b98c2663
Tyler Reckart <tyler.reckart@gmail.com>
2021-07-02 12:15:23 -0400

    substantial refactor

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,7 +1,19 @@
 (function () {
   document.addEventListener("DOMContentLoaded", (event) => {
-    (localStorage.getItem("mode") || "dark") === "dark"
-      ? document.querySelector("body").classList.add("dark")
-      : document.querySelector("body").classList.remove("dark");
+    const {
+      location: { pathname },
+    } = window;
+
+    let path = pathname.split("/")[1];
+
+    if (path.length === 0) {
+      path = "/";
+    }
+
+    const currentPage = [].filter.call(nav.children, (el) =>
+      [].includes.call(el.classList, path !== "/" ? path : "home")
+    )[0];
+    console.log(currentPage);
+    currentPage.classList.add("active");
   });
 })();
