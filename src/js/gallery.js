4a997642c7b380afbde81597ebd66b2bfc20f704
Tyler Reckart <tyler.reckart@gmail.com>
2021-10-19 18:57:53 -0400

    build out gallery keyboard interactions

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -73,22 +73,76 @@
 
   document.addEventListener("click", handlePopup);
 
+  function nextValidIndex(arr, nextIndex) {
+    console.log(arr, nextIndex);
+    const length = arr.length;
+
+    if (nextIndex >= length) {
+      return 0;
+    }
+
+    if (nextIndex < 0) {
+      return arr.length - 1;
+    }
+
+    return nextIndex;
+  }
+
   function handleKeyDown(event) {
     const { key } = event;
 
     const overlay = document.getElementById("gallery--popup--overlay");
 
     if (overlay) {
+      const image = document.getElementById("gallery--popup--image");
+
       if (key === "Escape") {
         overlay.classList.add("fadeOut");
-        const image = document.getElementById("gallery--popup--image");
-
         image.classList.add("slideDown");
-
         setTimeout(() => {
           document.body.removeChild(overlay);
           document.body.style.overflow = "auto";
-        }, 350);
+        }, 500);
+      }
+
+      const collection = [];
+      const images = document
+        .getElementById("gallery--image-wrapper")
+        .getElementsByTagName("img");
+
+      for (let i = 0; i < images.length; i++) {
+        collection[i] = images[i].src;
+      }
+
+      const { src } = image;
+      const index = collection.indexOf(src);
+
+      if (key === "ArrowRight") {
+        image.classList.add("slideOutRight");
+
+        setTimeout(() => {
+          image.classList.remove("slideOutRight");
+          image.src = collection[nextValidIndex(collection, index + 1)];
+          image.classList.add("slideInLeft");
+        }, 500);
+
+        setTimeout(() => {
+          image.classList.remove("slideInLeft");
+        }, 1000);
+      }
+
+      if (key === "ArrowLeft") {
+        image.classList.add("slideOutLeft");
+
+        setTimeout(() => {
+          image.classList.remove("slideOutLeft");
+          image.src = collection[nextValidIndex(collection, index - 1)];
+          image.classList.add("slideInRight");
+        }, 500);
+
+        setTimeout(() => {
+          image.classList.remove("slideInRight");
+        }, 1000);
       }
     }
   }
