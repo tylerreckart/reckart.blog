77bf7465b8e038d8324c9cbe09b9776bea9f78f6
Tyler Reckart <tyler.reckart@gmail.com>
2021-10-30 10:05:59 -0400

    continue working on gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,14 +1,10 @@
 (function () {
-  function buildStyles(el, style) {
-    for (const property in style) {
-      el.style[property] = style[property];
-    }
-  }
-
-  function buildOverlay() {
+  function buildLightbox() {
+    // Format the lightbox.
     const overlay = document.createElement("div");
+    overlay.id = "lightbox--overlay";
 
-    overlay.id = "gallery--popup--overlay";
+    // Trigger the transition animation.
     overlay.classList.add("fadeIn");
     setTimeout(() => {
       overlay.classList.remove("fadeIn");
@@ -17,72 +13,44 @@
     return overlay;
   }
 
-  function buildImage(parent, src, text) {
-    const imageWrapper = document.createElement("div");
-    imageWrapper.classList.add("image--wrapper");
+  function buildImage(parent, data) {
+    // Format the image wrapper.
+    const wrapper = document.createElement("div");
+    wrapper.id = "lightbox--image-wrapper";
+    wrapper.setAttribute(
+      "style",
+      "display:flex; flex-direction:column; align-items:flex-start; position:absolute; top:50%; transform:translateY(-50%) ;max-width:900px;"
+    );
 
+    // Format the image.
     const image = document.createElement("img");
     image.id = "gallery--popup--image";
-    image.src = src;
-    image.classList.add("slideUp");
+    image.src = data.src;
+    image.alt = data.alt;
+    image.setAttribute("style", "max-width:100%; max-height:90vh;");
 
+    // Format the image description.
     const description = document.createElement("p");
     description.setAttribute(
       "style",
-      "font-size:14px;line-height:14px;margin-bottom:0;"
+      "font-size:14px; line-height:14px; margin-top:24px; margin-bottom:0;"
     );
-    description.innerHTML = text;
+    description.innerHTML = data.description;
 
+    // Trigger the transition animation.
+    wrapper.classList.add("slideUp");
     setTimeout(() => {
-      image.classList.remove("slideUp");
+      wrapper.classList.remove("slideUp");
     }, 1000);
 
-    const imageStyleMap = {
-      display: "block",
-      position: "absolute",
-      top: "50%",
-      transform: "translateY(-50%)",
-      height: "100%",
-    };
-
-    buildStyles(image, imageStyleMap);
-    imageWrapper.appendChild(image);
-    parent.appendChild(imageWrapper);
-    parent.appendChild(description);
+    // Insert the target image and description into the image wrapper.
+    wrapper.appendChild(image);
+    wrapper.appendChild(description);
+    // Insert the image wrapper into the lightbox.
+    parent.appendChild(wrapper);
   }
-
-  function handlePopup(event) {
-    const { target } = event;
-    const {
-      classList: { value },
-    } = target;
-
-    if (event.target.id === "gallery--popup--overlay") {
-      target.classList.add("fadeOut");
-
-      const image = document.getElementById("gallery--popup--image");
-      image.classList.add("slideDown");
-
-      setTimeout(() => {
-        document.body.removeChild(target);
-        document.body.style.overflow = "auto";
-      }, 350);
-    }
-
-    if (value.includes("gallery--item")) {
-      const { src, dataset } = target;
-
-      const overlay = buildOverlay();
-      buildImage(overlay, src, dataset.description);
-      document.body.appendChild(overlay);
-      document.body.style.overflow = "hidden";
-    }
-  }
-
-  document.addEventListener("click", handlePopup);
 
   function nextValidIndex(arr, nextIndex) {
-    console.log(arr, nextIndex);
     const length = arr.length;
 
     if (nextIndex >= length) {
@@ -96,61 +64,120 @@
     return nextIndex;
   }
 
+  function nextImage(
+    wrapper,
+    image,
+    description,
+    nextSrc,
+    nextDescription,
+    direction
+  ) {
+    const inClass = `slideIn${direction === "left" ? "Right" : "Left"}`;
+    const outClass = `slideOut${direction === "left" ? "Left" : "Right"}`;
+
+    wrapper.classList.add(outClass);
+
+    setTimeout(() => {
+      wrapper.classList.remove(outClass);
+      image.src = nextSrc;
+      description.innerHTML = nextDescription;
+      wrapper.classList.add(inClass);
+    }, 500);
+
+    setTimeout(() => {
+      wrapper.classList.remove(inClass);
+    }, 1000);
+  }
+
+  function handlePopup(event) {
+    const { target } = event;
+    const {
+      classList: { value },
+    } = target;
+
+    if (event.target.id === "lightbox--overlay") {
+      target.classList.add("fadeOut");
+
+      const image = document.getElementById("lightbox--image-wrapper");
+      image.classList.add("slideDown");
+
+      setTimeout(() => {
+        document.body.removeChild(target);
+        document.body.style.overflow = "auto";
+      }, 350);
+    }
+
+    if (value.includes("gallery--item")) {
+      const {
+        src,
+        alt,
+        dataset: { description },
+      } = target;
+
+      const lightbox = buildLightbox();
+      buildImage(lightbox, { src, alt, description });
+
+      document.body.appendChild(lightbox);
+      document.body.style.overflow = "hidden";
+    }
+  }
+
+  document.addEventListener("click", handlePopup);
+
   function handleKeyDown(event) {
     const { key } = event;
 
-    const overlay = document.getElementById("gallery--popup--overlay");
+    const overlay = document.getElementById("lightbox--overlay");
 
     if (overlay) {
-      const image = document.getElementById("gallery--popup--image");
+      const wrapper = document.getElementById("lightbox--image-wrapper");
 
       if (key === "Escape") {
         overlay.classList.add("fadeOut");
-        image.classList.add("slideDown");
+        wrapper.classList.add("slideDown");
+
         setTimeout(() => {
           document.body.removeChild(overlay);
           document.body.style.overflow = "auto";
         }, 500);
       }
 
-      const collection = [];
       const images = document
         .getElementById("gallery--image-wrapper")
         .getElementsByTagName("img");
 
-      for (let i = 0; i < images.length; i++) {
-        collection[i] = images[i].src;
-      }
+      const collection = Array.from(images).map((i) => ({
+        src: i.src,
+        description: i.dataset.description,
+      }));
 
-      const { src } = image;
-      const index = collection.indexOf(src);
+      const image = wrapper.children[0];
+      const description = wrapper.children[1];
+
+      const index = collection.indexOf(
+        collection.find((i) => i.src === image.src)
+      );
 
       if (key === "ArrowRight") {
-        image.classList.add("slideOutRight");
-
-        setTimeout(() => {
-          image.classList.remove("slideOutRight");
-          image.src = collection[nextValidIndex(collection, index + 1)];
-          image.classList.add("slideInLeft");
-        }, 500);
-
-        setTimeout(() => {
-          image.classList.remove("slideInLeft");
-        }, 1000);
+        nextImage(
+          wrapper,
+          image,
+          description,
+          collection[nextValidIndex(collection, index + 1)].src,
+          collection[nextValidIndex(collection, index + 1)].description,
+          "right"
+        );
       }
 
       if (key === "ArrowLeft") {
-        image.classList.add("slideOutLeft");
-
-        setTimeout(() => {
-          image.classList.remove("slideOutLeft");
-          image.src = collection[nextValidIndex(collection, index - 1)];
-          image.classList.add("slideInRight");
-        }, 500);
-
-        setTimeout(() => {
-          image.classList.remove("slideInRight");
-        }, 1000);
+        nextImage(
+          wrapper,
+          image,
+          description,
+          collection[nextValidIndex(collection, index - 1)].src,
+          collection[nextValidIndex(collection, index - 1)].description,
+          "left"
+        );
       }
     }
   }
@@ -171,6 +198,8 @@
     }
   }
 
+  document.addEventListener("DOMContentLoaded", handleViewportConstraints);
+
   function handleResize(event) {
     const {
       target: {
@@ -188,6 +217,6 @@
       }
     }
   }
-  document.addEventListener("DOMContentLoaded", handleViewportConstraints);
+
   window.addEventListener("resize", handleResize);
 })();
