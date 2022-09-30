883d0e34f0582d982b6918fe47c287bc802cb1a2
Tyler Reckart <tyler.reckart@gmail.com>
2022-09-30 08:44:41 -0400

    start re-working gallery for print store

--- src/app/routes/photos.ts
+++ src/app/routes/photos.ts
@@ -9,19 +9,59 @@ const render = pug.compileFile(
   path.join(`${__dirname}/../../templates/photos.pug`)
 );
 
-const photos = [
-  { type: 'featured', src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/hyacinth.jpg' },
-  { type: 'gallery', photos: [
-    { src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/lorien-blog-2.jpg' },
-    { src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/disillusion.jpg' },
-  ]},
-  { type: 'featured', src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/L1003180-min.jpg' },
-  { type: 'featured', src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/leaf_scene_2-Edit-2-min.jpg' },
-  { type: 'gallery', photos: [
-    { src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/000474390003-2-min.jpg' },
-    { src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/000474390012-Edit-min.jpg' },
-  ]},
-]
+type GalleryImage = {
+  title: string;
+  year: number;
+  src: string;
+}
+
+type PhotoGallery = {
+  title: string;
+  colophon: string;
+  featuredImage: string;
+  images: GalleryImage[];
+}
+
+const gallery: PhotoGallery[] = [
+  {
+    title: 'Intimate Nature',
+    colophon: 'Much of my photography focuses on smaller, intimate scenes. The subjects in these scenes may often be overlooked.',
+    featuredImage: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/hyacinth.jpg',
+    images: [
+      { title: 'Clorophyll', year: 2022, src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/hyacinth.jpg' },
+    ],
+  },
+  {
+    title: 'Of Land',
+    colophon: '',
+    featuredImage: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/disillusion.jpg',
+    images: [
+      { title: 'Disillusion', year: 2022, src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/disillusion.jpg' },
+    ]
+  },
+  {
+    title: 'Of Water',
+    colophon: '',
+    featuredImage: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/lorien-blog-2.jpg',
+    images: [
+      { title: 'Lothlorien', year: 2022, src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/lorien-blog-2.jpg' },
+    ],
+  }
+];
+
+// const photos = [
+//   { type: 'featured', src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/hyacinth.jpg' },
+//   { type: 'gallery', photos: [
+//     { src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/lorien-blog-2.jpg' },
+//     { src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/disillusion.jpg' },
+//   ]},
+//   { type: 'featured', src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/L1003180-min.jpg' },
+//   { type: 'featured', src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/leaf_scene_2-Edit-2-min.jpg' },
+//   { type: 'gallery', photos: [
+//     { src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/000474390003-2-min.jpg' },
+//     { src: 'https://s3.us-east-2.amazonaws.com/reckart.blog-images/000474390012-Edit-min.jpg' },
+//   ]},
+// ]
 
 export default function buildPhotoGallery(
   // photos: Array<PostType>,
@@ -34,11 +74,11 @@ export default function buildPhotoGallery(
   }
 
   const archive = render({
-    photos,
+    gallery,
     ...config,
   });
 
-  fs.writeFile(`${outdir}/photos/index.html`, archive, (error: any): void => {
+  fs.writeFile(`${outdir}/photos/index.html`, archive, (error: Error | null): void => {
     if (error) {
       throw error;
     }
